25caa50c373cfaa204a25d2371568853b3745f57
Yjabbie <147781389+Yjabbie@users.noreply.github.com>
2023-11-13 02:31:37 -0500

    Update PsychBioScreen.py

--- PsychBioScreen.py
+++ PsychBioScreen.py
@@ -13,34 +13,54 @@ from PySide6.QtWidgets import (
     QGridLayout
 )
 
+mport sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow , QPushButton
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        """
-        def initUI(self):
-            self.label = Qtwidgets.QLabel(self)
-            self.label.setText("Psycology Sections")
-            self.label.setPointSize(30)
-            self.label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        """
-        
-        self.setWindowTitle("Psych Sections ")
-        #self.initUI()
-        Title = QLabel("Psych Sets")
-        font = Title.font()
-        font.setPointSize(45)
-        Title.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        
 
-        layout = QVBoxLayout()
-        Button = QGridLayout()
-        widget = QWidget()
-        widget.setLayout(layout)
+        self.setWindowTitle("Psych Section")
+        self.setGeometry(100,100,600,500)
+
+        widget = QLabel("Psychology")
+        widget2 = QLabel("Psych sets")
+        widget2.move(250,250)
+        font = widget.font()# <1>
+        font2 = widget2.font()
+        font2.setPointSize(15)
+        font.setPointSize(30)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)# <2>
+        
+        
+        self.setCentralWidget(widget2)
         self.setCentralWidget(widget)
+        Button = QPushButton("Cognitive ", self)
+        Button2 = QPushButton("Social ", self)
+        Button3 = QPushButton("Personality", self)
+        Button4 = QPushButton("Humanistic", self)
+        Button5 = QPushButton("Behaviorism", self)
+        Button6 = QPushButton("Forensic", self)
+        
+        Button.move(250,300)
+        Button2.move(250,330)
+        Button3.move(250,360)
+        Button4.move(250,390)
+        Button5.move(250,420)
+        Button6.move(250,450)
+     
 
-        layout.addWidget(Title)
-        #button.addButton()
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
 
 app = QApplication(sys.argv)
 
