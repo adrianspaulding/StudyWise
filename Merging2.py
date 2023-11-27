0166c8855787cd517a1e54d5aff1f14531e49371
Yjabbie <147781389+Yjabbie@users.noreply.github.com>
2023-11-27 11:33:03 -0500

    Update Merging2.py

--- Merging2.py
+++ Merging2.py
@@ -146,6 +146,43 @@ class MainWindow(QMainWindow):
         
         # Add all to a screen list to allow for switching
         self.screenList.addWidget(bioScreencontainer)
+
+
+        """
+        --------------
+        | Psy Screen |
+         --------------
+        """
+        self.setWindowTitle("Psych Section")
+        self.setGeometry(100,100,600,500)
+
+        widget = QLabel("Psychology")
+        widget2 = QLabel("Psych sets")
+        widget2.move(250,250)
+        font = widget.font()# <1>
+        font2 = widget2.font()
+        font2.setPointSize(15)
+        font.setPointSize(30)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)# <2>
+        
+        
+        self.setCentralWidget(widget2)
+        self.setCentralWidget(widget)
+        Button = QPushButton("Cognitive ", self)
+        Button2 = QPushButton("Social ", self)
+        Button3 = QPushButton("Personality", self)
+        Button4 = QPushButton("Humanistic", self)
+        Button5 = QPushButton("Behaviorism", self)
+        Button6 = QPushButton("Forensic", self)
+        
+        Button.move(250,300)
+        Button2.move(250,330)
+        Button3.move(250,360)
+        Button4.move(250,390)
+        Button5.move(250,420)
+        Button6.move(250,450
+        self.screenList.addWidget(PsyScreencontainer)
         
         
         """
